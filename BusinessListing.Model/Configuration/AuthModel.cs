3f4cc89775c332b257f12ea7a2f595c2d1d6d154
Alokan Oladipo <AlokanOladipo@hotmail.com>
2021-06-06 05:26:11 +0100

    AuthModel

---
+++ BusinessListing.Model/Configuration/AuthModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessListing.Model.Configuration
+{
+    class AuthModel
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+
+        public string ResponseCode { get; set; }
+
+        public bool Success { get; set; }
+        public List<String> Errors { get; set; }
+    }
+}
---
+++ BusinessListing.Model/DTO/Responses/UserLoginResponse.cs
@@ -0,0 +1,13 @@
+﻿using BusinessListing.Model.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessListing.Model.DTO.Responses
+{
+    class UserLoginResponse: AuthModel
+    {
+    }
+}
