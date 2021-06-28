6b48347be4269e1b5c1ba50f79cb2c0ac34ad586
Alokan Oladipo <AlokanOladipo@hotmail.com>
2021-06-28 04:57:16 +0100

    repo uow

--- ApiManagement/Startup.cs
+++ ApiManagement/Startup.cs
@@ -1,19 +1,14 @@
 using ApiManagement.Configuration;
+using BusinessListing.BL;
+using BusinessListing.BL.IConfiguration;
 using BusinessListing.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ApiManagement
 {
@@ -41,6 +36,8 @@ namespace ApiManagement
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiManagement", Version = "v1" });
             });
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
