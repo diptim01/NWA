b2d2830da4b03c3b7680afe726d5bccdfacf1bcc
Alokan Oladipo <AlokanOladipo@hotmail.com>
2021-06-28 03:27:31 +0100

    initials

--- ApiManagement/Controllers/AuthenticationController.cs
+++ ApiManagement/Controllers/AuthenticationController.cs
@@ -12,36 +12,9 @@ namespace ApiManagement.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
-        // GET: api/<AuthenticationController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        public AuthenticationController()
         {
-            return new string[] { "value1", "value2" };
-        }
 
-        // GET api/<AuthenticationController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<AuthenticationController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<AuthenticationController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<AuthenticationController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
